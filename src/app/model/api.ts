9951c42d3556040db788b1ebbb50ba515356d69b
Bohdan <bohdan@klevercompany.com>
2023-02-20 21:09:24 +0200

    commented api

--- src/app/model/api.ts
+++ src/app/model/api.ts
@@ -1,43 +1,41 @@
-import originalFetch from "isomorphic-fetch";
-import fetchBuilder from "fetch-retry";
-import { notify } from "app/providers/with-notifications";
-import { ApiCategories, ApiProducts, ApiRecipes, ApiFridge } from "shared/api";
+// import fetchBuilder from 'fetch-retry';
+// import originalFetch from 'isomorphic-fetch';
 
-export class Api {
-  static Categories = ApiCategories;
-  static Recipes = ApiRecipes;
-  static Products = ApiProducts;
-  static Fridge = ApiFridge;
-  // static Auth = ApiAuth;
+// import { ApiCategories, ApiProducts, ApiRecipes, ApiFridge } from 'shared/api';
 
-  static handleErrorMessage(obj: {
-    message?: string;
-    msg?: string;
-    code?: number;
-  }) {
-    if (!obj.message && !obj.msg) return;
-    if (obj.code === 4003) return;
+// import { notify } from 'app/providers/with-notifications';
 
-    notify({
-      message: obj.msg || obj.message || "",
-      type: obj.code || "10000",
-    });
-  }
+// export class Api {
+//   static Categories = ApiCategories;
+//   static Recipes = ApiRecipes;
+//   static Products = ApiProducts;
+//   static Fridge = ApiFridge;
+//   // static Auth = ApiAuth;
 
-  static async fetchRetry(path: string, props: RequestInit) {
-    try {
-      const options = {
-        retries: 3,
-        retryDelay: 3000,
-      };
+//   static handleErrorMessage(obj: { message?: string; msg?: string; code?: number }) {
+//     if (!obj.message && !obj.msg) return;
+//     if (obj.code === 4003) return;
 
-      const fetch = fetchBuilder(originalFetch, options);
-      const data = await fetch(path, props);
-      const json = await data.json();
-      return json;
-    } catch (err) {
-      console.error(err, path, props);
-      return null;
-    }
-  }
-}
+//     notify({
+//       message: obj.msg || obj.message || '',
+//       type: obj.code || '10000',
+//     });
+//   }
+
+//   static async fetchRetry(path: string, props: RequestInit) {
+//     try {
+//       const options = {
+//         retries: 3,
+//         retryDelay: 3000,
+//       };
+
+//       const fetch = fetchBuilder(originalFetch, options);
+//       const data = await fetch(path, props);
+//       const json = await data.json();
+//       return json;
+//     } catch (err) {
+//       console.error(err, path, props);
+//       return null;
+//     }
+//   }
+// }
